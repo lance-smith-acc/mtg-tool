eaadd825732b8743fdf0fec0a8d5a86ed74fc6f2
Lance Smith <sirlancepants@gmail.com>
2020-12-23 15:24:24 -0600

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import Whiteboard from './whiteboard'
+import styled from 'styled-components';
 
 function App() {
+
+  const AppWrapper = styled.div`
+    margin:0 auto;
+    width:80%;
+    height:100vh;
+    display:flex;
+    flex-direction:column;
+    justify-content:center;
+    background:gray;
+  `
+
+  const Header = styled.header`
+    width:70%;
+    height:20vh;
+    margin:0% 15%;
+    background:slategrey;
+    display:flex;
+    flex-direction:column;
+    justify-content:center;
+    align-items:center;
+  `
+  const Body = styled.body`
+    width:70%;
+    height:80vh;
+    background:white;
+    align-self:center;
+  `
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppWrapper className="App">
+      <Header className="App-header">
+       <h1 className="Title">MTG Card Tool</h1>
+       <Whiteboard />
+      </Header>
+      <Body>
+          
+      </Body>
+    </AppWrapper>
   );
 }
 
---
+++ src/whiteboard.js
@@ -0,0 +1,26 @@
+import './App.css';
+import styled from 'styled-components'
+
+export default function Whiteboard() {
+    const Whiteboard = styled.div`
+        width:100%;
+        height:100%;
+        padding-top:2%;
+        justify-content:center;
+        background:papayawhip;
+        align-items:center;
+        text-align:center;
+    `
+    return(
+        <Whiteboard>
+            <h2 className="WhiteboardTitle">Whiteboard</h2>
+            <h3>Things to Do</h3>
+            <ul>
+                <li>Make a search bar</li>
+                <li>Make API Calls</li>
+                <li>Make a card object</li>
+                <li>Display card objects</li>
+            </ul>
+        </Whiteboard>
+    )
+}
